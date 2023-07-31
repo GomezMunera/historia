a169ca82350d0ba4de934e395c3644eb257ba8e0
GomezMunera <gomezmunera@corum.org.co>
2023-07-31 18:25:15 -0500

    deployment

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,7 @@
+RANDOM=$$
 git pull
 bundle exec jekyll build
-docker build . -t corumcorp/historia:latest
-docker push corumcorp/historia:latest
-microk8s kubectl delete -f deployment/deployment.yaml
+docker build . -t corumcorp/historia:${$RANDOM}
+docker push corumcorp/historia:${$RANDOM}
+sed 's/version/${$RANDOM}/g' deployment-template.yaml > deployment.yaml
 microk8s kubectl apply -f deployment/deployment.yaml
