0d55f0ba7efd085430442c640cb7f368d498f6cd
GomezMunera <gomezmunera@corum.org.co>
2023-07-31 18:44:41 -0500

    replacement

--- deployment.sh
+++ deployment.sh
@@ -5,5 +5,6 @@ git pull
 bundle exec jekyll build
 docker build . -t corumcorp/historia:${version}
 docker push corumcorp/historia:${version}
-sed 's/version/${version}/g' deployment/deployment-template.yaml > deployment/deployment.yaml
+replacement = 's/version/${version}/g'
+sed  $replacement deployment/deployment-template.yaml > deployment/deployment.yaml
 microk8s kubectl apply -f deployment/deployment.yaml
